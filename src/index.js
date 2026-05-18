d1c07ee32f16f67f02d79e0bdb4d92caf77ac141
user_bots <usertv0707@gmail.com>
2026-05-17 23:34:39 -0500

    Add keep-alive self-ping every 5 min

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const http = require("http");
+const https = require("https");
 const { createClient } = require("./client");
 const { initDB } = require("./database");
 
@@ -8,6 +9,12 @@ http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
   res.end("Bot is running");
 }).listen(PORT, () => console.log(`[HTTP] Health check server on port ${PORT}`));
+
+const SELF_URL = process.env.RENDER_EXTERNAL_URL || `http://localhost:${PORT}`;
+setInterval(() => {
+  const caller = SELF_URL.startsWith("https") ? https : http;
+  caller.get(SELF_URL, (res) => res.on("data", () => {}));
+}, 5 * 60 * 1000);
 
 async function main() {
   await initDB();
